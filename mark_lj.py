70d81f063760eab18971d2e5c8e8517db32abc75
kimberleydillon <kim.alexandra.dillon@gmail.com>
2016-02-26 23:32:01 -0800

    used innerHTML instead of text to get images sources as well as text

--- mark_lj.py
+++ mark_lj.py
@@ -7,6 +7,7 @@ driver.get("http://mark--mywords.livejournal.com/")
 data  = driver.find_elements_by_css_selector ("tbody > tr > td > table.s2-entrytext > tbody > tr > td:not(.comments)")
 
 for datum in data: 
-    print datum.text
+    print datum.get_attribute('innerHTML')
+
 
 driver.close()
